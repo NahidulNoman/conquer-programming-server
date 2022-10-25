d8ad0426c28ce66eb5207814e68b13ccfbdb220f
NahidulNoman <nahidulna5@gmail.com>
2022-10-25 20:11:58 +0600

    api create done

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000 ;
+app.use(cors());
+
+app.get('/', (req,res) => {
+    res.send('conquer server is running')
+})
+
+app.listen(port , () => {
+    console.log('conquer server is running ...')
+})
