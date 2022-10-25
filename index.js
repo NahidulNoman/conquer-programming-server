7527c58c37c73062c6716e73e0437b9995f6de68
NahidulNoman <nahidulna5@gmail.com>
2022-10-25 21:10:06 +0600

    cached node_modules

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
 app.use(cors());
+
+const categoryCourse = require('./Course&Details/CategoryCourse.json');
+const detailCourse = require('./Course&Details/detailsCourse.json');
 
 app.get('/', (req,res) => {
     res.send('conquer server is running')
+})
+app.get('/courseCategory', (req,res) => {
+    res.send(categoryCourse);
+})
+app.get('/details', (req,res) => {
+    res.send(detailCourse)
 })
 
 app.listen(port , () => {
