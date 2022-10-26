9af796b7b84229b4f6dfebf85a5bc6288e42c71d
NahidulNoman <nahidulna5@gmail.com>
2022-10-26 10:50:18 +0600

    do meaningfull comment

--- index.js
+++ index.js
@@ -7,20 +7,29 @@ app.use(cors());
 const categoryCourse = require('./Course&Details/CategoryCourse.json');
 const detailCourse = require('./Course&Details/detailsCourse.json');
 
+// test server
 app.get('/', (req,res) => {
     res.send('conquer server is running')
 });
+
+// get side category API
 app.get('/courseCategory', (req,res) => {
     res.send(categoryCourse);
 });
+
+// get all detail category API
 app.get('/details', (req,res) => {
     res.send(detailCourse)
 });
+
+// get unique data for unique category
 app.get('/courseDetails/:id', (req,res) => {
     const id = parseInt(req.params.id);
     const selected = detailCourse.find( n => n.id === id)
     res.send(selected)
 });
+
+// server is running 
 app.listen(port , () => {
     console.log('conquer server is running ...')
 });
